0480e6813383650439520fae7efef05ceabae03e
stefan <sdecimelli97@gmail.com>
2020-05-25 19:12:48 -0400

    Switched to mongo url, better for compose secrets

--- backend/src/main/java/com/decimelli/backend/MongoService.java
+++ backend/src/main/java/com/decimelli/backend/MongoService.java
@@ -1,8 +1,5 @@
 package com.decimelli.backend;
 
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -12,7 +9,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Provider;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @ApplicationScoped
@@ -21,34 +17,13 @@ public class MongoService {
     private MongoClient mongoClient;
 
     @Inject
-    @ConfigProperty(name = "mongo.admin.username")
-    private String user;     // the user name
-
-    @Inject
-    @ConfigProperty(name = "mongo.admin.source")
-    private String source;   // the source where the user is defined
-
-    @Inject
-    @ConfigProperty(name = "mongo.admin.password")
-    private String pw; // the password as a character array
-
-    @Inject
-    @ConfigProperty(name = "mongo")
-    private Provider<String> host;
-
-    @Inject
-    @ConfigProperty(name = "mongo.port")
-    private int port;
+    @ConfigProperty(name = "MONGODB-CONNECTION-URL")
+    private Provider<String> url;
 
     @PostConstruct
     private void init() {
         System.out.println("Connecting to MongoDB...");
-        List<ServerAddress> servers = Arrays.asList(new ServerAddress(host.get(), port));
-        MongoCredential credential = MongoCredential.createCredential(user, source, pw.toCharArray());
-        mongoClient = MongoClients.create(MongoClientSettings.builder()
-                .applyToClusterSettings(builder -> builder.hosts(servers))
-                .credential(credential)
-                .build());
+        mongoClient = MongoClients.create(url.get());
         System.out.println("...connected to MongoDB: " + mongoClient.getClusterDescription());
     }
 
@@ -63,4 +38,5 @@ public class MongoService {
         mongoClient.getDatabase(dbname).listCollectionNames().into(collectionNames);
         return collectionNames;
     }
+
 }
